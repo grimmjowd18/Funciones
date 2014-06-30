5971c3788d2fcc700f4cb0afac2671d1bbee381d
Usuario <Usuario@Pc>
2014-06-30 00:56:42 -0500

    prueba 2

--- JavaApplication8/src/javaapplication8/JavaApplication8.java
+++ JavaApplication8/src/javaapplication8/JavaApplication8.java
@@ -16,7 +16,7 @@ public class JavaApplication8 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // haciendo prueba blablablablablabla
     }
     
 }
